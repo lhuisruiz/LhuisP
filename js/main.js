c3ea2286d88767151df8773b52230e033e16031e
lhuisruiz <84159346+lhuisruiz@users.noreply.github.com>
2023-11-16 20:53:22 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@ const typed = new Typed('.typed', {
 	strings: [
 		'<i class="let">Lhuis</i>',
 		'<i class="let">Fotografo</i>',
-		'<i class="let">Lhuis Ruiz</i>',
-		
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	typeSpeed: 100, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 40, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
